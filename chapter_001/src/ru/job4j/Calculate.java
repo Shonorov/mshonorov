f08c53424a18e5134a9d52ad9b5c736dcbc5b1bf
Shonorov <shonorov@gmail.com>
2018-02-23 14:29:40 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/ru/job4j/Calculate.java
+++ chapter_001/src/ru/job4j/Calculate.java
@@ -1,7 +1,12 @@
 package ru.job4j;
 
+/**
+* Prints Hello world.
+* @author Mikhail Shonorov (shonorov@gmail.com)
+*/
+
 public class Calculate {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world!");
 	}
 }
---
+++ chapter_001/src/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+﻿/**
+* Package for calculate task.
+*
+* @author Mikhail Shonorov (shonorov@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
