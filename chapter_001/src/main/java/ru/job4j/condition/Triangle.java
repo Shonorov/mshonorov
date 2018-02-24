9987ca428eb3c9a85f463f8037748582bc12c048
Shonorov <shonorov@gmail.com>
2018-02-24 18:07:08 +0300

    3.3. Вычисление площади треугольника

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,68 @@
+package ru.job4j.condition;
+
+/**
+ * Triangle area calculation.
+ * @author MShonorov (shonorov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Triangle {
+
+    private Point a;
+    private Point b;
+    private Point c;
+
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    /**
+     * Метод вычисления полупериметра по длинам сторон.
+     *
+     * Формула.
+     *
+     * (ab + ac + bc) / 2
+     *
+     * @param ab расстояние между точками a b
+     * @param ac расстояние между точками a c
+     * @param bc расстояние между точками b c
+     * @return Перимент.
+     */
+    public double period(double ab, double ac, double bc) {
+        return (a.distanceTo(b) + a.distanceTo(c) + b.distanceTo(c)) / 2;
+    }
+
+    /**
+     * Метод должен вычислить площадь треугольника.
+     *
+     * @return Вернуть прощадь, если треугольник существует или -1, если треугольника нет.
+     */
+    public double area() {
+        double rsl = -1;
+        double ab = this.a.distanceTo(this.b);
+        double ac = this.a.distanceTo(this.c);
+        double bc = this.b.distanceTo(this.c);
+        double p = this.period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            // написать формулу для расчета площади треугольника.
+            rsl = Math.pow(p * (p - ab) * (p - ac) * (p - bc), 0.5);
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     *
+     * Подумайте какое надо написать условие, чтобы определить можно ли построить треугольник.
+     *
+     * @param ab Длина от точки a b.
+     * @param ac Длина от точки a c.
+     * @param bc Длина от точки b c.
+     * @return if triangle exists.
+     */
+    private boolean exist(double ab, double ac, double bc) {
+        return (ab + ac > bc) ? true : false;
+    }
+}
