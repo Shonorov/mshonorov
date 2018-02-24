880861a0c3e91d31c20da1d7cd85c37564277ebe
Shonorov <shonorov@gmail.com>
2018-02-24 14:20:17 +0300

    3.2. Конвертер валюты.

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Converter.java
@@ -0,0 +1,49 @@
+package ru.job4j.calculator;
+
+/**
+ * Корвертор валюты.
+ */
+public class Converter {
+
+    /**
+     * Money exchange rates.
+     */
+    private static final int EURO_EXCHANGE_RATE = 70;
+    private static final int DOLLAR_EXCHANGE_RATE = 60;
+
+    /**
+     * Конвертируем рубли в евро.
+     * @param value рубли.
+     * @return Евро.
+     */
+    public int rubleToEuro(int value) {
+        return value / EURO_EXCHANGE_RATE;
+    }
+
+    /**
+     * Конвертируем рубли в доллары.
+     * @param value рубли.
+     * @return Доллоры
+     */
+    public int rubleToDollar(int value) {
+        return value / DOLLAR_EXCHANGE_RATE;
+    }
+
+    /**
+     * Dollar to rubles exchange converter.
+     * @param value dollar.
+     * @return rubles.
+     */
+    public int dollarToRuble(int value) {
+        return value * DOLLAR_EXCHANGE_RATE;
+    }
+
+    /**
+     * Euro to rubles exchange converter.
+     * @param value euro.
+     * @return rubles.
+     */
+    public int euroToRuble(int value) {
+        return value * EURO_EXCHANGE_RATE;
+    }
+}
