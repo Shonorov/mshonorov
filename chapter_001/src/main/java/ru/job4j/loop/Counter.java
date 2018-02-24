c8e62146e340aac86b7d28f97fc2dfd4d4ff9bb5
Shonorov <shonorov@gmail.com>
2018-02-24 18:34:52 +0300

    4.1. Подсчет суммы чётных чисел в диапазоне

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,25 @@
+package ru.job4j.loop;
+
+/**
+ * @author MShonorov (shonorov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Counter {
+
+    /**
+     * Get sum of evens in range.
+     * @param start of the range.
+     * @param finish of the range.
+     * @return sum.
+     */
+    public int add(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
