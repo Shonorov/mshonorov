5d4d429636718b316e48f7489bffd5a1b5957373
Shonorov <shonorov@gmail.com>
2018-02-25 15:30:17 +0300

    5.2. Перевернуть массив

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+/**
+ * @author MShonorov (shonorov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Turn {
+
+    /**
+     * Turn array values order.
+     * @param inArray input array.
+     * @return output array.
+     */
+    public int[] back(int[] inArray) {
+        for (int i = 0; i <= inArray.length / 2 - 1; i++) {
+            int current = inArray[i];
+            inArray[i] = inArray[inArray.length - i - 1];
+            inArray[inArray.length - i - 1] = current;
+        }
+        return inArray;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author MShonorov (shonorov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class TurnTest {
+
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        //напишите здесь тест, проверяющий переворот массива с чётным числом элементов, например {2, 6, 1, 4}.
+        int[] inArray = new int[] {2, 6, 1, 4};
+        int[] expect = new int[] {4, 1, 6, 2};
+        Turn turn = new Turn();
+        int[] result = turn.back(inArray);
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+        //напишите здесь тест, проверяющий переворот массива с нечётным числом элементов, например {1, 2, 3, 4, 5}.
+        int[] inArray = new int[] {1, 2, 3, 4, 5};
+        int[] expect = new int[] {5, 4, 3, 2, 1};
+        Turn turn = new Turn();
+        int[] result = turn.back(inArray);
+        assertThat(result, is(expect));
+    }
+}
