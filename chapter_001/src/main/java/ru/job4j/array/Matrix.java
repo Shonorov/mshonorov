35b4596617b08fa9cd89c49bcad3a149feb05b7c
Shonorov <shonorov@gmail.com>
2018-02-25 16:12:59 +0300

    5.4. Двухмерный массив. Таблица умножения.

---
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+/**
+ * @author MShonorov (shonorov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Matrix {
+
+    /**
+     * Fills matrix of fixed size with multiplied indexes.
+     * @param size matrix size.
+     * @return matrix.
+     */
+    int[][] multiple(int size) {
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return matrix;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author MShonorov (shonorov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class MatrixTest {
+
+    @Test
+    public void whenIndex9And9x8ThenReturn72() {
+        Matrix matrix = new Matrix();
+        int[][] result = matrix.multiple(9);
+        int expected9x8 = 72;
+        assertThat(result[8][7], is(expected9x8));
+    }
+
+    @Test
+    public void whenIndex9And5x6ThenReturn30() {
+        Matrix matrix = new Matrix();
+        int[][] result = matrix.multiple(9);
+        int expected5x6 = 30;
+        assertThat(result[4][5], is(expected5x6));
+    }
+}
