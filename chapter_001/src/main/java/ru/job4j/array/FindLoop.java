5a106bbe73bd3839a0343562105a290e8b2557b4
Shonorov <shonorov@gmail.com>
2018-02-25 15:05:47 +0300

    5.1. Классический поиск перебором

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+/**
+ * @author MShonorov (shonorov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class FindLoop {
+
+    /**
+     * Return index of given element in array.
+     * @param data array.
+     * @param el element.
+     * @return index.
+     */
+    public int indexOf(int[] data, int el) {
+
+        int rsl = -1; // если элемента нет в массиве, то возвращаем -1.
+
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author MShonorov (shonorov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class FindLoopTest {
+
+    @Test
+    public void whenElementPresentInArray() {
+        FindLoop findLoop = new FindLoop();
+        int[] array = new int[] {1, 5, 3, 4, 10, 15};
+        int element = 10;
+        int result = findLoop.indexOf(array, element);
+        int expect = 4;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenElementNotPresentInArray() {
+        FindLoop findLoop = new FindLoop();
+        int[] array = new int[] {1, 5, 3, 4, 10, 15};
+        int element = 20;
+        int result = findLoop.indexOf(array, element);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+}
+
+
