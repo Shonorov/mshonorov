4bbc5e0b06992a21abc24784fb389a36e213218e
Shonorov <shonorov@gmail.com>
2018-03-10 20:30:20 +0300

    1. JaCoCo. Процент покрытие тестами

--- chapter_003/src/main/java/ru/job4j/sort/User.java
+++ chapter_003/src/main/java/ru/job4j/sort/User.java
@@ -27,10 +27,13 @@ public class User implements Comparable<User> {
 
     @Override
     public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
+        return "User{"
+                + "name='"
+                + name
+                + '\''
+                + ", age="
+                + age
+                + '}';
     }
 
     @Override
