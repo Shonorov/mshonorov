d2857d4dd99f7daa14105dcd84c078cfbaf51da0
Shonorov <shonorov@gmail.com>
2018-03-14 18:48:49 +0300

    5.1.1. Итератор для двухмерного массива int[][]

---
+++ chapter_101/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -0,0 +1,55 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Jagged array custom iterator.
+ * Iterates each element in each sub array.
+ * @author MShonorov (shonorov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class MatrixIterator implements Iterator {
+    /**
+     * Iterable array.
+     * Current position in array.
+     * Array total length.
+     */
+    private final int[][] value;
+    private int position = 0;
+    private int arrLength;
+
+    public MatrixIterator(int[][] value) {
+        this.value = value;
+        int length = 0;
+        for (int i = 0; i < value.length; i++) {
+            length += value[i].length;
+        }
+        this.arrLength = length;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return position < arrLength;
+    }
+
+    @Override
+    public Object next() {
+        if (arrLength == 0) {
+            throw new NoSuchElementException();
+        }
+        int current = 0;
+        for (int i = 0; i < value.length; i++) {
+            for (int j = 0; j < value[i].length; j++) {
+                if (current == position) {
+                    position++;
+                    return value[i][j];
+                } else {
+                    current++;
+                }
+            }
+        }
+        return null;
+    }
+}
---
+++ chapter_101/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for chapter_101 study project.
+*
+* @author Mikhail Shonorov (shonorov@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
