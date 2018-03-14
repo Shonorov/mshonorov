c22498666c49785f1a3cc2dc2b8522c6fd0d539b
Shonorov <shonorov@gmail.com>
2018-03-15 00:08:49 +0300

    5.1.4. Создать convert(Iterator<Iterator>)

---
+++ chapter_101/src/main/java/ru/job4j/iterator/Converter.java
@@ -0,0 +1,46 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Converts Iterator of iterators<Integer> to single Iterator<Integer>.
+ * @author MShonorov (shonorov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Converter {
+
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<Integer>() {
+            /**
+             * Current iterator to process.
+             */
+            private Iterator<Integer> current = it.next();
+
+            @Override
+            public boolean hasNext() {
+                boolean result = false;
+                if (current.hasNext()) {
+                    result = current.hasNext();
+                } else if (it.hasNext()) {
+                    current = it.next();
+                    result = current.hasNext();
+                }
+                return result;
+            }
+
+            @Override
+            public Integer next() {
+                if (current.hasNext()) {
+                    return current.next();
+                } else if (it.hasNext()) {
+                    current = it.next();
+                    return current.next();
+                } else {
+                    throw new NoSuchElementException();
+                }
+            }
+        };
+    }
+}
