9dd93ac0111ab0815f8aba580e306911cac9823c
Shonorov <shonorov@gmail.com>
2018-03-20 22:11:00 +0300

    3. Переопределить только hashCode

--- chapter_101/src/main/java/ru/job4j/map/User.java
+++ chapter_101/src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,8 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.Objects;
+
 /**
  * User for map examples.
  * @author MShonorov (shonorov@gmail.com)
@@ -16,6 +18,22 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        User user = (User) o;
+//        return children == user.children &&
+//                Objects.equals(name, user.name) &&
+//                Objects.equals(birthday, user.birthday);
+//    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, children, birthday);
     }
 }
 
