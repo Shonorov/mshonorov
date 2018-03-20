43301118c64c384d5678dedfb42ffa1e0eede8a3
Shonorov <shonorov@gmail.com>
2018-03-20 22:11:53 +0300

    4. Переопределить только equals

--- chapter_101/src/main/java/ru/job4j/map/User.java
+++ chapter_101/src/main/java/ru/job4j/map/User.java
@@ -20,21 +20,20 @@ public class User {
         this.birthday = birthday;
     }
 
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        User user = (User) o;
-//        return children == user.children &&
-//                Objects.equals(name, user.name) &&
-//                Objects.equals(birthday, user.birthday);
-//    }
-
     @Override
-    public int hashCode() {
-
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(birthday, user.birthday);
     }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(name, children, birthday);
+//    }
 }
 
 
