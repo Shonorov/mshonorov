9acf9771bee8738bf6ce3ee2e56c436e37dd9cf0
Shonorov <shonorov@gmail.com>
2018-04-01 22:26:48 +0300

    3. Реализовать собственный механизм блокировок Lock

---
+++ chapter_102/src/main/java/ru/job4j/wait/SimpleLock.java
@@ -0,0 +1,46 @@
+package ru.job4j.wait;
+/**
+ * Simple lock realization.
+ * @author MShonorov (shonorov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SimpleLock {
+    /**
+     * Thread owner of lock.
+     * Lock state.
+     */
+    private Thread owner = null;
+    private volatile boolean unlocked = true;
+
+    public boolean isUnlocked() {
+        return unlocked;
+    }
+
+    /**
+     * Enable lock and set owner thread.
+     */
+    public void lock() {
+        if (unlocked && owner == null) {
+            owner = Thread.currentThread();
+            unlocked = false;
+            System.out.println(Thread.currentThread().getName() + " locked.");
+        } else {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    /**
+     * Disable lock and clear owner thread.
+     */
+    public void unlock() {
+        if (!unlocked && owner == Thread.currentThread()) {
+            owner = null;
+            unlocked = true;
+            System.out.println(Thread.currentThread().getName() + " unlocked.");
+        } else {
+            System.out.println(Thread.currentThread().getName() + " unlock failed.");
+            throw new IllegalMonitorStateException();
+        }
+    }
+}
---
+++ chapter_102/src/test/java/ru/job4j/wait/SimpleLockTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.wait;
+import org.junit.Test;
+
+/**
+ * @author MShonorov (shonorov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SimpleLockTest {
+
+    class CustomLockExample implements Runnable {
+        private SimpleLock lock = new SimpleLock();
+        @Override
+        public void run() {
+            try {
+                lock.lock();
+                Thread.sleep(500);
+                lock.unlock();
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + " lock interrupted.");
+            }
+        }
+    }
+
+    @Test
+    public void whenLocked() throws InterruptedException {
+        CustomLockExample example = new CustomLockExample();
+        Thread thread1 = new Thread(example);
+        Thread thread2 = new Thread(example);
+        Thread thread3 = new Thread(example);
+        thread1.start();
+        thread2.start();
+        Thread.sleep(1000);
+        thread3.start();
+        thread1.join();
+        thread2.join();
+        thread3.join();
+    }
+}
