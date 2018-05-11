e8e7ff2f742586a219fd71882f3b9942fd207ccc
Shonorov <shonorov@gmail.com>
2018-05-11 22:00:03 +0300

    Игра бомбермен[#48933]

--- chapter_102/src/test/java/ru/job4j/wait/StringWriterTest.java
+++ chapter_102/src/test/java/ru/job4j/wait/StringWriterTest.java
@@ -3,6 +3,8 @@ package ru.job4j.wait;
 import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isOneOf;
+
 import java.util.concurrent.locks.ReentrantLock;
 
 public class StringWriterTest {
@@ -14,14 +16,14 @@ public class StringWriterTest {
         Thread thread2 = new Thread(new StringWriter(lock, 2));
         try {
             thread1.start();
-            Thread.sleep(100);
             thread2.start();
             thread1.join();
             thread2.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        String expect = "111111111122222222221111111111222222222211111111112222222222";
-        assertThat(StringWriter.content, is(expect));
+        String expect1 = "111111111122222222221111111111222222222211111111112222222222";
+        String expect2 = "222222222211111111112222222222111111111122222222221111111111";
+        assertThat(StringWriter.content, isOneOf(expect1, expect2));
     }
 }
