5b108ff507749da265a1f2a163da696af14ff74e
Shonorov <shonorov@gmail.com>
2018-05-11 21:49:30 +0300

    Игра бомбермен[#48933]

--- chapter_102/src/main/java/ru/job4j/wait/StringWriter.java
+++ chapter_102/src/main/java/ru/job4j/wait/StringWriter.java
@@ -1,14 +1,11 @@
 package ru.job4j.wait;
-import net.jcip.annotations.GuardedBy;
+
 import net.jcip.annotations.ThreadSafe;
 
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * Simple multithreaded string writer.
+ * Simple multithreaded string digit appender.
  * @author MShonorov (shonorov@gmail.com)
  * @version $Id$
  * @since 0.1
@@ -16,27 +13,47 @@ import java.util.concurrent.locks.ReentrantLock;
 @ThreadSafe
 public class StringWriter implements Runnable {
 
-    @GuardedBy("this")
-    private static final String content = "";
-    private ReentrantLock lock;
-    private String digit;
+    /**
+     * Common string content.
+     * Class lock.
+     * Digit to append.
+     * Append repeat count.
+     */
+    public static String content = "";
+    private final ReentrantLock lock;
+    private int digit;
+    private static final int REPEAT = 3;
 
     public StringWriter(ReentrantLock lock, int digit) {
         this.lock = lock;
-        this.digit = String.valueOf(digit);
+        this.digit = digit;
     }
 
     @Override
     public void run() {
-        //https://stackoverflow.com/questions/15530484/how-to-switch-between-two-thread-back-and-forth
+        for (int i = 0; i < REPEAT; i++) {
+            synchronized (lock) {
+                append(digit);
+                try {
+                    lock.notify();
+                    lock.wait(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
-    public synchronized void append(int digit) {
-        System.out.println(Thread.currentThread().getName() + " append");
-
-    }
-
-    public synchronized String getContent() {
-        return this.content;
+    /**
+     * Append ten digits.
+     * @param digit digit to append.
+     */
+    private synchronized void append(int digit) {
+        String current = String.valueOf(digit);
+        StringBuilder builder = new StringBuilder(content);
+        for (int i = 0; i < 10; i++) {
+            builder.append(current);
+        }
+        content = builder.toString();
     }
 }
--- chapter_102/src/test/java/ru/job4j/wait/StringWriterTest.java
+++ chapter_102/src/test/java/ru/job4j/wait/StringWriterTest.java
@@ -1,25 +1,27 @@
 package ru.job4j.wait;
 
 import org.junit.Test;
-
-import java.util.concurrent.Semaphore;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class StringWriterTest {
+
     @Test
     public void whenAddThenReturnString() {
-
         ReentrantLock lock = new ReentrantLock();
         Thread thread1 = new Thread(new StringWriter(lock, 1));
         Thread thread2 = new Thread(new StringWriter(lock, 2));
-        thread1.start();
-        thread2.start();
-//         try {
-//             thread1.join();
-//             thread2.join();
-//         } catch (InterruptedException e) {
-//             e.printStackTrace();
-//         }
-
+        try {
+            thread1.start();
+            Thread.sleep(1);
+            thread2.start();
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String expect = "111111111122222222221111111111222222222211111111112222222222";
+        assertThat(StringWriter.content, is(expect));
     }
 }
