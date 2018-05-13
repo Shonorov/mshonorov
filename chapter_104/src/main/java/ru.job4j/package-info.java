7996e6fe1fabbfedc44a001e11fd7f583e0e5227
Shonorov <shonorov@gmail.com>
2018-05-13 19:19:01 +0300

    Создать web проект

---
+++ chapter_104/src/main/java/ru.job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for chapter_104 study project.
+*
+* @author Mikhail Shonorov (shonorov@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
---
+++ chapter_104/src/main/java/ru.job4j/servlets/EchoServlet.java
@@ -0,0 +1,18 @@
+package ru.job4j.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class EchoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("Hello World!");
+        writer.flush();
+    }
+}
