061fd7dd180bea8356a4c839ca7a56a923454f55
Shonorov <shonorov@gmail.com>
2018-06-22 17:08:59 +0300

    Лямбды и шаблон wrapper.

--- chapter_201/src/main/java/ru/job4j/todolist/ToDoListRepository.java
+++ chapter_201/src/main/java/ru/job4j/todolist/ToDoListRepository.java
@@ -5,13 +5,12 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
-
 import java.io.Closeable;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 /**
  * Repository layout for ToDoList web application.
@@ -42,24 +41,37 @@ public class ToDoListRepository implements Closeable {
     }
 
     /**
+     * Wrapper template method.
+     * @param command function to execute.
+     * @param <T> generic function parameter value.
+     * @return method return type.
+     */
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = factory.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            return command.apply(session);
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            tx.commit();
+            session.close();
+        }
+    }
+
+    /**
      * Find all items in database.
      * @return list of all items.
      */
     public List<Item> getAllItems() {
-        List<Item> result = new ArrayList<>();
-        Session session = factory.openSession();
-        Transaction transaction = session.beginTransaction();
-        try {
-            result = session.createQuery("from Item").list();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            e.printStackTrace();
-        } finally {
-            transaction.commit();
-            session.close();
-            result.sort(comparator);
-        }
-        return result;
+        return this.tx(
+                session -> {
+                    List<Item> result = session.createQuery("from Item").list();
+                    result.sort(comparator);
+                    return result;
+                }
+        );
     }
 
     /**
@@ -67,20 +79,13 @@ public class ToDoListRepository implements Closeable {
      * @return list of undone items.
      */
     public List<Item> getOnlyCurrentItems() {
-        List<Item> result = new ArrayList<>();
-        Session session = factory.openSession();
-        Transaction transaction = session.beginTransaction();
-        try {
-            result = session.createQuery("from Item where done = false").list();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            e.printStackTrace();
-        } finally {
-            transaction.commit();
-            session.close();
-            result.sort(comparator);
-        }
-        return result;
+        return this.tx(
+                session -> {
+                    List<Item> result = session.createQuery("from Item where done = false").list();
+                    result.sort(comparator);
+                    return result;
+                }
+        );
     }
 
     /**
@@ -89,21 +94,12 @@ public class ToDoListRepository implements Closeable {
      * @return true if success.
      */
     public boolean createItem(String description) {
-        boolean result = false;
-        Session session = factory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Item newItem = new Item(description);
-        try {
-            session.save(newItem);
-            result = true;
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            e.printStackTrace();
-        } finally {
-            transaction.commit();
-            session.close();
-        }
-        return result;
+        return this.tx(
+                session -> {
+                    session.save(new Item(description));
+                    return true;
+                }
+        );
     }
 
     /**
@@ -113,23 +109,15 @@ public class ToDoListRepository implements Closeable {
      * @return true if success.
      */
     public boolean setDone(String id, Boolean done) {
-        boolean result = false;
-        Session session = factory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Item item = findByID(id).get();
-        item.setId(Integer.valueOf(id));
-        item.setDone(done);
-        try {
-            session.update(item);
-            result = true;
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            e.printStackTrace();
-        } finally {
-            transaction.commit();
-            session.close();
-        }
-        return result;
+        return this.tx(
+                session -> {
+                    Item item = findByID(id).get();
+                    item.setId(Integer.valueOf(id));
+                    item.setDone(done);
+                    session.update(item);
+                    return true;
+                }
+        );
     }
 
     /**
@@ -138,13 +126,12 @@ public class ToDoListRepository implements Closeable {
      * @return optional of item.
      */
     public Optional<Item> findByID(String id) {
-        Session session = factory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Query query = session.createQuery("from Item where id=:userid");
-        query.setParameter("userid", Integer.valueOf(id));
-        Optional<Item> result = Optional.of((Item) query.getSingleResult());
-        transaction.commit();
-        session.close();
-        return result;
+        return this.tx(
+                session -> {
+                    Query query = session.createQuery("from Item where id=:userid");
+                    query.setParameter("userid", Integer.valueOf(id));
+                    return Optional.of((Item) query.getSingleResult());
+                }
+        );
     }
 }
