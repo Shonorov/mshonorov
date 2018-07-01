31f6a257e78123b585187de3ab1d349f0c4da434
shonorov <shonorov@gmail.com>
2018-07-01 20:58:44 +0300

    Реализовать площадку продаж машин.

--- chapter_201a/src/main/java/ru/job4j/cars/controller/ItemCreateServlet.java
+++ chapter_201a/src/main/java/ru/job4j/cars/controller/ItemCreateServlet.java
@@ -21,6 +21,30 @@ public class ItemCreateServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Hit!");
+        System.out.println(req.getAttribute("header"));
+        System.out.println(req.getAttribute("text"));
+        System.out.println(req.getAttribute("price"));
+        System.out.println(req.getAttribute("drive"));
+        System.out.println(req.getAttribute("manufactured"));
+        System.out.println(req.getAttribute("photo"));
+        System.out.println(req.getAttribute("manufacturer"));
+        System.out.println(req.getAttribute("country"));
+        System.out.println(req.getAttribute("model"));
+        System.out.println(req.getAttribute("releasedate"));
+        System.out.println(req.getAttribute("manufacturing"));
+        System.out.println(req.getAttribute("enginetype"));
+        System.out.println(req.getAttribute("enginevolume"));
+        System.out.println(req.getAttribute("enginepower"));
+        System.out.println(req.getAttribute("enginemilage"));
+        System.out.println(req.getAttribute("bodytype"));
+        System.out.println(req.getAttribute("bodycolor"));
+        System.out.println(req.getAttribute("wheelside"));
+        System.out.println(req.getAttribute("gearboxtype"));
+        System.out.println(req.getAttribute("gearcount"));
+
+
+
         req.getRequestDispatcher("/WEB-INF/views/item_list.html").forward(req, resp);
     }
 }
