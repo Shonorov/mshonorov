6898af5cc816e276b3e30f236a604594ade114db
shonorov <shonorov@gmail.com>
2018-07-10 16:58:20 +0300

    Реализовать площадку продаж машин.

--- chapter_201a/src/main/java/ru/job4j/cars/model/Engine.java
+++ chapter_201a/src/main/java/ru/job4j/cars/model/Engine.java
@@ -1,5 +1,7 @@
 package ru.job4j.cars.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 /**
  * Engine model for car shop web application.
@@ -10,11 +12,16 @@ import javax.persistence.*;
 
 @Entity
 @Table (name = "engine")
+@EqualsAndHashCode
+@NoArgsConstructor
+@ToString
+@Getter @Setter
 public class Engine {
 
     @Id
     @GeneratedValue (strategy = GenerationType.SEQUENCE)
     @Column (name = "id")
+    @EqualsAndHashCode.Exclude
     private Integer id;
 
     @Column (name = "type")
@@ -29,93 +36,10 @@ public class Engine {
     @Column (name = "mileage")
     private Integer mileage;
 
-    public Engine() {
-    }
-
     public Engine(String type, Double volume, Integer power, Integer mileage) {
         this.type = type;
         this.volume = volume;
         this.power = power;
         this.mileage = mileage;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Double getVolume() {
-        return volume;
-    }
-
-    public void setVolume(Double volume) {
-        this.volume = volume;
-    }
-
-    public Integer getPower() {
-        return power;
-    }
-
-    public void setPower(Integer power) {
-        this.power = power;
-    }
-
-    public Integer getMileage() {
-        return mileage;
-    }
-
-    public void setMileage(Integer mileage) {
-        this.mileage = mileage;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof Engine)) {
-            return false;
-        }
-
-        Engine engine = (Engine) o;
-
-        if (!type.equals(engine.type)) {
-            return false;
-        }
-        if (!volume.equals(engine.volume)) {
-            return false;
-        }
-        if (!power.equals(engine.power)) {
-            return false;
-        }
-        return mileage.equals(engine.mileage);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = type.hashCode();
-        result = 31 * result + volume.hashCode();
-        result = 31 * result + power.hashCode();
-        result = 31 * result + mileage.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Engine{" + "id=" + id
-                + ", type='" + type
-                + ", volume=" + volume
-                + ", power=" + power
-                + ", mileage=" + mileage + '}';
     }
 }
