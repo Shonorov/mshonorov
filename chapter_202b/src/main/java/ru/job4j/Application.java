2319ce22dcf2e2014b69cab22d9207c3bead6af4
shonorov <shonorov@gmail.com>
2018-07-27 16:10:50 +0300

    Сделать java -jar запускаемым проект площадку машин.

---
+++ chapter_202b/src/main/java/ru/job4j/Application.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
