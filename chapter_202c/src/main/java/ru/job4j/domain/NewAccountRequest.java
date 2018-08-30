d839d107790f5dc395885e36925a5c8c28e47ee0
shonorov <shonorov@gmail.com>
2018-08-30 17:17:35 +0300

    Тестовое задание.

--- chapter_202c/src/main/java/ru/job4j/domain/NewAccountRequest.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.domain;
-
-import lombok.Data;
-
-@Data
-public class NewAccountRequest {
-
-    private String accountId;
-}
--- chapter_202c/src/main/resources/static/scripts.js
+++ chapter_202c/src/main/resources/static/scripts.js
@@ -1,12 +1,21 @@
-function openaccount(account) {
-    // var formData = JSON.stringify($("#usercreate").serializeArray());
-    console.log(account);
+function openaccount() {
+    var acc = $("#accountId").val();
+    var data = {AccountId:acc};
     $.ajax({
         type: "POST",
-        url: "account",
-        data: {"accountId":account},
-        success: function () {},
-        dataType: JSON,
-        contentType : "application/json"
+        url: "/account",
+        dataType: "JSON",
+        contentType : "application/json",
+        data: JSON.stringify(data),
+        success: function (response) {
+            console.log(response);
+            $("#result").empty().append(response.description);
+            $("#password").empty().append(response.password);
+        },
+        error: function (response) {
+            console.log(response.responseJSON.description);
+            $("#result").empty().append(response.responseJSON.description);
+            $("#password").empty();
+        }
     })
 }
