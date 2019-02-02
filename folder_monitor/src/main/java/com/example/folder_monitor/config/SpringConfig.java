e6db8b61c999f840f8a69af87be80593659d1db6
shonorov <shonorov@gmail.com>
2019-02-02 19:31:13 +0300

    CallerRunsPolicy

--- folder_monitor/src/main/java/com/example/folder_monitor/config/SpringConfig.java
+++ folder_monitor/src/main/java/com/example/folder_monitor/config/SpringConfig.java
@@ -2,8 +2,6 @@ package com.example.folder_monitor.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -15,7 +13,9 @@ public class SpringConfig {
     @Bean(name = "myexecutor")
     TaskExecutor getTaskExecutor() {
         ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
-        threadPoolTaskExecutor.setCorePoolSize(3);
+        threadPoolTaskExecutor.setCorePoolSize(2);
+        threadPoolTaskExecutor.setMaxPoolSize(2);
+        threadPoolTaskExecutor.setQueueCapacity(0);
         threadPoolTaskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         return threadPoolTaskExecutor;
     }
